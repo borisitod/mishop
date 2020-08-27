2601180a677394fe5a49fce19ed6fb5ea881324f
bdchen <chenjunchi414@gmail.com>
2020-08-27 21:36:36 +1000

    Interface error interception

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+
+//Interface error interception
+axios.interceptors.response.use((response) => {
+  let res = response.data
+
+  if (res.status == 0) {
+    return res.data
+  } else if (res.status == 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
