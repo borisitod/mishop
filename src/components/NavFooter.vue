1c85478549db1b0ab3f10449266b3031f02cb697
bdchen <chenjunchi414@gmail.com>
2020-09-18 21:27:55 +1000

    nav footer implement

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,9 +1,60 @@
 <template>
-  <div></div> 
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt />
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href target="_blank">个人主页</a>
+    </div>
+    <div class="copyright">Copyright ©2020 <span class="domain"> mishop.com All Rights Reserved. </span></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'nav-footer'
-}
+  name: "nav-footer",
+};
 </script>
+
+<style lang="scss">
+@import "./../assets/scss/config.scss";
+
+.footer {
+  height: 234px;
+  border-top: 4px solid $colorA;
+  background-color: $colorB;
+  color: $colorD;
+  font-size: $fontI;
+  text-align: center;
+
+  .footer-logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+
+  .footer-link {
+    a {
+      color: $colorD;
+      display: inline-block;
+    }
+    span {
+      margin: 0 10px;
+    }
+    margin-bottom: 13px;
+  }
+
+  .copyright {
+    .domain {
+      color: $colorA;
+    }
+  }
+
+}
+</style>
