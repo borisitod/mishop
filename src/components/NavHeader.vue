d3a9eee97c876a1e30da5b2bcf895dbbe664a29f
bdchen <chenjunchi414@gmail.com>
2020-09-19 12:16:35 +1000

    service bar implement

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -277,6 +277,7 @@ export default {
             box-shadow: 0px 7px 7px 0px rgba(0, 0, 0, 0.11);
             z-index: 10;
             transition: all 0.5s;
+            background-color: $colorG;
 
             .product {
               float: left;
